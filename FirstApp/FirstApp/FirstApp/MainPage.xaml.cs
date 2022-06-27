46e9729a5bbbdb399c613f00129c15bf5e99dd3d
王飞箭 <wangfeijianhao@163.com>
2022-06-27 17:08:27 +0800

    first demo

---
+++ FirstApp/FirstApp/FirstApp/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FirstApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int Count = 0;
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Count++;
+            ((Button)sender).Text = $"You clicked {Count} times.";
+        }
+
+    }
+}
