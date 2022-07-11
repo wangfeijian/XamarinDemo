8d52636a742f393a153c9159552b7adb1d1623a7
王飞箭 <wangfeijianhao@163.com>
2022-07-11 09:31:02 +0800

    优化app时间及编辑框适应问题

--- Notes/Notes/Notes.Android/Resources/Resource.designer.cs
+++ Notes/Notes/Notes.Android/Resources/Resource.designer.cs
@@ -14,7 +14,7 @@ namespace Notes.Droid
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.2.4.160")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.3.99.16")]
 	public partial class Resource
 	{
 		
